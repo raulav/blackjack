794cba499fe1e3bdeac3950a2b374b8ab8ca6143
raul <raul@rauls-MacBook-Air.local>
2019-10-24 14:19:49 -0500

    first commit prueba

--- src/App.js
+++ src/App.js
@@ -4,23 +4,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button></button>
+      <button></button>
+      <button></button>
+      <PlayerCard/>
+      <DealerCard/>
+      <Outcome></Outcome>
     </div>
   );
 }
 
+
+const PlayerCard = () => {
+  return (
+    <div>
+      <h1>Player's Score</h1>
+    </div>
+  );
+}
+
+const DealerCard = () => {
+  return (
+    <div>
+      <h1>Dealer's Score</h1>
+    </div>
+  );
+}
+
+const Outcome = () => {
+  return (
+    <div>
+      <h1>Winnder/loser</h1>
+    </div>
+  )
+}
 export default App;
